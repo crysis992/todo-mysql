941e5409881c1e1a312f82951c4c9f8050e710b8
crysis992 <crysis992@gmail.com>
2023-09-05 13:57:32 +0200

    Added sql-template-tag package

--- src/components/todo/TodoEntry.tsx
+++ src/components/todo/TodoEntry.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from "@/lib/auth";
 import prisma from "@/lib/db"
 import TodoList from "./TodoList";
+import sql, { empty, join, raw } from "sql-template-tag";
 
 async function TodoEntry() {
     const session = await getCurrentUser();
@@ -36,6 +37,14 @@ async function TodoEntry() {
         }),
         prisma.category.findMany()
     ])
+
+    // Raw SQL query test
+    // const query = sql`SELECT Todolist.title, Todolist.id, Todolist.createdAt, Category.title AS category, (SELECT Todolist.userId FROM Todolist WHERE Todolist.id = id LIMIT 1) AS test, (SELECT User.email FROM User WHERE User.id = test LIMIT 1) AS blub FROM Todolist INNER JOIN User ON Todolist.userId = User.id LEFT JOIN TodoEntry ON TodoEntry.TodolistId = Todolist.id LEFT JOIN Category ON TodoEntry.categoryId = Category.id WHERE Todolist.userId=2 ORDER BY TodoEntry.createdAt DESC LIMIT 2;`
+    // const test = await prisma.$queryRaw(query);
+    // console.log("==TEST==")
+    // console.log(test);
+
+
 
     return (
         <>
